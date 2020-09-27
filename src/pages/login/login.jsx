f78f970c8d407d7cb7f64273b4cf2a34ae2d48e9
Strange <strange@qq.com>
2020-09-28 00:18:27 +0800

    9.28

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 import "./css/login.less"
 import logo from './imgs/logo2.png'
-
 import { Form, Input, Button} from 'antd';
-const { Item } = Form
-const {Password} = Input
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+
+
+
 export default class Login extends Component {
+    //密码的验证器
+    checkPrice = (rule, value) => {
+        
+        if (value.length >= 4 &&value.length <= 12) {
+          return Promise.resolve();
+        }else if(/^\w+$/.test(value)){
+            return Promise.reject('密码必须由英文、数字、下划线组成');
+        }else 
+        return Promise.reject('密码为4-12位');
+      };
+    
+    
+    handleSubmit = () => { 
+        alert('表单提交了')
+
+    }
     render() {
         return (
             <div className="login">
@@ -15,24 +33,49 @@ export default class Login extends Component {
                 </header>
                 <section>
                     <h1>用户登录</h1>
+                    
 
-                    <Form onSubmit>
-                        <Item>
-                            <Input  placeholder="用户名"size="230px"/>
-                        </Item>
+                    <Form
+                        onSubmit={this.handleSubmit}
+                        name="normal_login"
+                        className="login-form"
+                        
+                       
 
-                        <Item>
-                            <Password placeholder="密码" size="230px"/>
-                        </Item>
+                    >
+                        <Form.Item
+                            name="username"
+                            rules={[{ required: true, message: '用户名不能为空'},
+                            {min:4,message:'用户名不能少于4位'},
+                            {max:12,message:'用户名不能大于12位'},
+                            {pattern:/^\w+$/,message:"用户名必须由英文、数字、下划线组成"}]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[{ validator: this.checkPrice}]}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                type="password"
+                                placeholder="密码"
+                            />
+                        </Form.Item>
+                        
 
-
-
-                        <Item>
-                            <Button type="primary" className="login-sub-btn">
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
                                 登录
                             </Button>
-                        </Item>
+        
+                        </Form.Item>
                     </Form>
+
+
+
+
+
 
                 </section>
             </div>
